b593642ea88ea06adcce9c53b249df31203995ff
j-faust <85812553+j-faust@users.noreply.github.com>
2023-01-30 18:21:34 -0500

    fixed seed file

--- seed/reviewSeed.js
+++ seed/reviewSeed.js
@@ -3,29 +3,17 @@ const { Review } = require('../model');
 const reviewData = [
     {
         movie_name: "test",
-        rating: 7,
-        review_comment: "testtesttesttesttesttesttest",
-        user_id: 1,
-    },
-    {
-        movie_name: "test",
-        rating: 9,
-        review_comment: "testtesttesttesttesttesttest",
-        user_id: 2,
-    },
-    {
-        movie_name: "test",
         rating: 5,
-        review_comment: "testtesttesttesttesttesttest",
+        review_comment: "testtesttesttest",
         user_id: 1,
     },
     {
-        movie_name: "test",
-        rating: 3,
-        review_comment: "testtesttesttesttesttesttest",
+        movie_name: "test2",
+        rating: 9,
+        review_comment: "testesttesttesttest",
         user_id: 2,
-    },
-]
+    }
+];
 
 const seedReview = () => Review.bulkCreate(reviewData);
 
